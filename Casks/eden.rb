b8b6f8d66372e5345f71480af36950c77c5a7a23
고범석 <kobums@naver.com>
2026-08-06 15:44:25 +0900

    eden: 저장소 URL kobums/eden으로 갱신

--- Casks/eden.rb
+++ Casks/eden.rb
@@ -2,10 +2,10 @@ cask "eden" do
   version "0.1.4"
   sha256 "fe6179182cb09c9c7ec56a114ee809199792fecdab8061f4373ac2ff98485133"
 
-  url "https://github.com/kobums/terminal/releases/download/v#{version}/eden-#{version}.zip"
+  url "https://github.com/kobums/eden/releases/download/v#{version}/eden-#{version}.zip"
   name "eden"
   desc "여러 터미널의 장점을 모은 macOS 네이티브 터미널"
-  homepage "https://github.com/kobums/terminal"
+  homepage "https://github.com/kobums/eden"
 
   livecheck do
     url :url
