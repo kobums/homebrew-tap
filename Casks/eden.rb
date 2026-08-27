b17d37da2a0987f05c3ae89776c1018a86b795fd
고범석 <kobums@naver.com>
2026-08-27 11:54:16 +0900

    eden 0.1.5

--- Casks/eden.rb
+++ Casks/eden.rb
@@ -1,6 +1,6 @@
 cask "eden" do
-  version "0.1.4"
-  sha256 "fe6179182cb09c9c7ec56a114ee809199792fecdab8061f4373ac2ff98485133"
+  version "0.1.5"
+  sha256 "d1cd0fa1442a0fbb5a6f19ef8e8e55444a42ee784f51a46469594ea17aad2f6b"
 
   url "https://github.com/kobums/eden/releases/download/v#{version}/eden-#{version}.zip"
   name "eden"
